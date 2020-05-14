8d6f1c738f11169d01c99c03ef045b51bdb646bf
ALee008 <nomansland008@googlemail.com>
2020-05-14 20:29:17 +0200

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,3 @@
+export DATABASE_URL=postgres://fhhtpgbxwpqsjm:d704a219ffcdbd6365011a695389cef418253d6fd4a95a1d4869a5ccb86c8ca5@ec2-54-247-169-129.eu-west-1.compute.amazonaws.com:5432/d59rbde2kjbhrt
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
